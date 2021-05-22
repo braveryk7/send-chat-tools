f3482c56d2060f4aedd36d9df814f29a836706c4
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2021-05-22 16:05:32 +0900

    add php version judgment

---
+++ send-chat-tools.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Plugin Name: Send Chat Tools
+ * Plugin URI:	https://www.braveryk7.com/
+ * Description:	
+ * Version:		0.0.1
+ * Author:		Ken-chan
+ * Author URI:	https://twitter.com/braveryk7
+ * Text Domain:	send-chat-tools
+ * Domain Path:	/languages
+ * License:		GPL2
+ * 
+ * @author Ken-chan
+ * @package WordPress
+ * @subpackage Send Chat Tools
+ * @since 0.0.1
+ */
