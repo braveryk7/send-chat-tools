29c070387d9723b15fb25121063c9c5d8fca3f39
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2021-05-22 16:09:01 +0900

    add php version judgment

--- send-chat-tools.php
+++ send-chat-tools.php
@@ -1,17 +1,45 @@
 <?php
 /**
  * Plugin Name: Send Chat Tools
- * Plugin URI:	https://www.braveryk7.com/
- * Description:	
- * Version:		0.0.1
- * Author:		Ken-chan
- * Author URI:	https://twitter.com/braveryk7
- * Text Domain:	send-chat-tools
- * Domain Path:	/languages
- * License:		GPL2
- * 
+ * Plugin URI:  https://www.braveryk7.com/
+ * Description:
+ * Version:     0.0.1
+ * Author:      Ken-chan
+ * Author URI:  https://twitter.com/braveryk7
+ * Text Domain: send-chat-tools
+ * Domain Path: /languages
+ * License:     GPL2
+ *
  * @author Ken-chan
  * @package WordPress
  * @subpackage Send Chat Tools
  * @since 0.0.1
  */
+
+declare( strict_type = 1 );
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit( 'You do not have access rights.' );
+}
+
+load_plugin_textdomain( 'send-chat-tools', false, basename( dirname( __FILE__ ) ) . '/languages' );
+
+require_once dirname( __FILE__ ) . '/class/class-judgment-php-version.php';
+
+$require_php_version  = '7.3.0';
+$get_php_version_bool = new Judgment_Php_Version();
+if ( false === $get_php_version_bool->judgment( $require_php_version ) ) {
+	require_once ABSPATH . 'wp-admin/includes/plugin.php';
+	if ( is_plugin_active( plugin_basename( __FILE__ ) ) ) {
+		if ( is_admin() ) {
+			require_once dirname( __FILE__ ) . '/modules/cancel-activate.php';
+			cancel_activate();
+		}
+		deactivate_plugins( plugin_basename( __FILE__ ) );
+	} else {
+		echo '<p>' . esc_html_e( 'Send Chat Tools requires at least PHP 7.3.0 or later.', 'send-chat-tools' ) . esc_html_e( 'Please upgrade PHP.', 'admin-bar-tools' ) . '</p>';
+		exit;
+	}
+} elseif ( true === $get_php_version_bool->judgment( $require_php_version ) ) {
+}
+
