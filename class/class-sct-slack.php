c6cb09ade465b98c5b2f433b7c618912b9df5df4
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2021-05-25 17:57:32 +0900

    add self send process

--- class/class-sct-slack.php
+++ class/class-sct-slack.php
@@ -24,41 +24,46 @@ class Sct_Slack {
 		require_once dirname( __FILE__ ) . '/class-sct-encryption.php';
 
 		global $wpdb;
-		$url              = get_option( 'sct_slack_webhook_url' );
-		$site_name        = get_bloginfo( 'name' );
-		$site_url         = get_bloginfo( 'url' );
-		$comment          = get_comment( $wpdb->insert_id );
-		$comment_approved = $comment->comment_approved;
-		$article_title    = get_the_title( $comment->comment_post_ID );
-		$article_url      = get_permalink( $comment->comment_post_ID );
-		$approved_url     = admin_url() . 'comment.php?action=approve&c=' . $comment->comment_ID;
+		$comment     = get_comment( $wpdb->insert_id );
+		$send_author = get_option( 'sct_send_slack_author' );
 
-		if ( '1' === $comment_approved ) {
-			$comment_status = esc_html__( 'Approved', 'send-chat-tools' );
-		} elseif ( '0' === $comment_approved ) {
-			$comment_status = esc_html__( 'Unapproved', 'send-chat-tools' ) . '<<' . $approved_url . '|' . esc_html__( 'Click here to approve', 'send-chat-tools' ) . '>>';
-		} elseif ( 'spam' === $comment_approved ) {
-			$comment_status = esc_html__( 'Spam', 'send-chat-tools' );
+		if ( ( '0' === $send_author ) || ( '1' === $send_author && '0' === $comment->user_id ) ) {
+			$url              = get_option( 'sct_slack_webhook_url' );
+			$site_name        = get_bloginfo( 'name' );
+			$site_url         = get_bloginfo( 'url' );
+			$comment_approved = $comment->comment_approved;
+			$article_title    = get_the_title( $comment->comment_post_ID );
+			$article_url      = get_permalink( $comment->comment_post_ID );
+			$approved_url     = admin_url() . 'comment.php?action=approve&c=' . $comment->comment_ID;
+
+			if ( '1' === $comment_approved ) {
+				$comment_status = esc_html__( 'Approved', 'send-chat-tools' );
+			} elseif ( '0' === $comment_approved ) {
+				$comment_status = esc_html__( 'Unapproved', 'send-chat-tools' ) . '<<' . $approved_url . '|' . esc_html__( 'Click here to approve', 'send-chat-tools' ) . '>>';
+			} elseif ( 'spam' === $comment_approved ) {
+				$comment_status = esc_html__( 'Spam', 'send-chat-tools' );
+			}
+
+			$message = [
+				'text' =>
+					$site_name . '(' . $site_url . ')' . esc_html__( 'new comment has been posted.', 'send-chat-tools' ) . "\n\n" .
+					esc_html__( 'Commented article:', 'send-chat-tools' ) . '<' . $article_url . '|' . $article_title . ">\n" .
+					esc_html__( 'Author:', 'send-chat-tools' ) . $comment->comment_author . '<' . $comment->comment_author_email . ">\n" .
+					esc_html__( 'Date and time:', 'send-chat-tools' ) . $comment->comment_date . "\n" .
+					esc_html__( 'Text:', 'send-chat-tools' ) . "\n" . $comment->comment_content . "\n\n" .
+					esc_html__( 'Comment URL:', 'send-chat-tools' ) . $article_url . '#comment-' . $comment->comment_ID . "\n\n" .
+					esc_html__( 'Comment Status:', 'send-chat-tools' ) . $comment_status .
+					'send author?:' . $send_author . 'comment User ID:' . $comment->user_id,
+			];
+
+			$options = [
+				'headers' => [
+					'Content-Type: application/json;charset=utf-8',
+				],
+				'body'    => wp_json_encode( $message ),
+			];
+
+			$result = wp_remote_post( Sct_Encryption::decrypt( $url ), $options );
 		}
-
-		$message = [
-			'text' =>
-				$site_name . '(' . $site_url . ')' . esc_html__( 'new comment has been posted.', 'send-chat-tools' ) . "\n\n" .
-				esc_html__( 'Commented article:', 'send-chat-tools' ) . '<' . $article_url . '|' . $article_title . ">\n" .
-				esc_html__( 'Author:', 'send-chat-tools' ) . $comment->comment_author . '<' . $comment->comment_author_email . ">\n" .
-				esc_html__( 'Date and time:', 'send-chat-tools' ) . $comment->comment_date . "\n" .
-				esc_html__( 'Text:', 'send-chat-tools' ) . "\n" . $comment->comment_content . "\n\n" .
-				esc_html__( 'Comment URL:', 'send-chat-tools' ) . $article_url . '#comment-' . $comment->comment_ID . "\n\n" .
-				esc_html__( 'Comment Status:', 'send-chat-tools' ) . $comment_status,
-		];
-
-		$options = [
-			'headers' => [
-				'Content-Type: application/json;charset=utf-8',
-			],
-			'body'    => wp_json_encode( $message ),
-		];
-
-		$result = wp_remote_post( Sct_Encryption::decrypt( $url ), $options );
 	}
 }
