fd59728bba0f98bd0ee4b222a20a4126b08dc806
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2021-05-26 01:58:40 +0900

    add delete option

---
+++ class/class-sct-connect-database.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Admin settings page.
+ *
+ * @author Ken-chan
+ * @package WordPress
+ * @subpackage Send Chat Tools
+ * @since 0.0.1
+ */
+
+declare( strict_type = 1 );
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit( 'You do not have access rights.' );
+}
+
+/**
+ * Connect Database.
+ */
+class Sct_Connect_Database {
+	/**
+	 * Delete wp_option.
+	 */
+	public static function delete_db() {
+		global $wpdb;
+
+		delete_option( 'sct_use_slack' );
+		delete_option( 'sct_slack_webhook_url' );
+		delete_option( 'sct_send_slack_author' );
+		delete_option( 'sct_iv' );
+		delete_option( 'sct_use_user_id' );
+	}
+}
