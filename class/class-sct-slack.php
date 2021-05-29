7cbcca6b2f841ac800d386cb245dff7f712126e8
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2021-05-30 00:28:11 +0900

    add case of error

--- class/class-sct-slack.php
+++ class/class-sct-slack.php
@@ -63,6 +63,14 @@ class Sct_Slack {
 			];
 
 			$result = wp_remote_post( Sct_Encryption::decrypt( $url ), $options );
+			update_option( 'sct_slack_log', $result );
+
+			$states_code = $result['response']['code'];
+			if ( 200 !== $states_code ) {
+				require_once dirname( __FILE__ ) . '/class-sct-error-mail.php';
+				$send_mail = new Sct_Error_Mail( $states_code, $wpdb->insert_id );
+				$send_mail->make_contents();
+			}
 		}
 	}
 }
