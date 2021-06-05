92201fd1aaa073e63b1879fa3b4a92096e405677
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2021-06-05 23:47:07 +0900

    add plugins update logic

--- class/class-sct-check-update.php
+++ class/class-sct-check-update.php
@@ -18,10 +18,25 @@ if ( ! defined( 'ABSPATH' ) ) {
  * Check Update WordPress core, theme, and plugin.
  */
 class Sct_Check_Update {
+
+	/**
+	 * WordPress hook.
+	 * Add WP-Cron.
+	 */
+	public function __construct() {
+		add_action( 'sct_plugin_check', [ $this, 'check_plugins' ] );
+		if ( ! wp_next_scheduled( 'sct_plugin_check' ) ) {
+			$my_time   = gmdate( 'Y-m-d 03:00:00', strtotime( current_datetime()->format( 'Y-m-d H:i:s' ) ) );
+			$cron_time = strtotime( -1 * (int) current_datetime()->format( 'O' ) / 100 . 'hour', strtotime( $my_time ) );
+
+			wp_schedule_event( $cron_time, 'daily', 'sct_plugin_check' );
+		}
+	}
+
 	/**
 	 * Plugins.
 	 */
-	public static function check_plugins(): array {
+	public function check_plugins() {
 		global $wpdb;
 
 		$get_plugin_states = get_option( '_site_transient_update_plugins' );
@@ -29,15 +44,41 @@ class Sct_Check_Update {
 		if ( ! empty( $get_plugin_states->response ) ) {
 			$update_plugins = $get_plugin_states->response;
 			foreach ( $update_plugins as $key ) {
-				$new_version          = get_file_data( WP_PLUGIN_DIR . '/' . $key->plugin, [ 'version' => 'Version' ] );
+				$path                 = WP_PLUGIN_DIR . '/' . $key->plugin;
+				$plugin_date          = get_file_data(
+					$path,
+					[
+						'name'    => 'Plugin Name',
+						'version' => 'Version',
+					]
+				);
 				$return[ $key->slug ] = [
-					'name'            => $key->slug,
-					'path'            => WP_PLUGIN_DIR . '/' . $key->plugin,
-					'current_version' => $new_version['version'],
+					'name'            => $plugin_date['name'],
+					'attribute'       => 'plugin',
+					'path'            => $path,
+					'current_version' => $plugin_date['version'],
 					'new_version'     => $key->new_version,
 				];
 			}
 		}
-		return $return;
+		update_option( 'sct_plugin_check', $return );
+		$this->check_tools( $return, $id );
+	}
+
+	/**
+	 * Check tools use status and call.
+	 *
+	 * @param array $return Update info.
+	 */
+	private function check_tools( array $return ) {
+		if ( '1' === get_option( 'sct_use_slack' ) ) {
+			Sct_Slack::create_update_contents( $return, $id );
+		}
+		if ( '1' === get_option( 'sct_use_chatwork' ) ) {
+			add_action( 'comment_post', 'Sct_Chatwork::send_chatwork' );
+		}
+		if ( '1' === get_option( 'sct_use_discord' ) ) {
+			add_action( 'comment_post', 'Sct_Discord::send_discord' );
+		}
 	}
 }
