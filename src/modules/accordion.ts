dbd9476d20bdf614766ec67b55b82a7f3e626d7d
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2021-06-10 01:20:08 +0900

    lint

--- src/modules/accordion.ts
+++ src/modules/accordion.ts
@@ -1,7 +1,7 @@
-export function toggle (eleme: HTMLElement)  {
-	if(eleme !== null) {
+export function toggle( eleme: HTMLElement ) {
+	if ( eleme !== null ) {
 		const content: HTMLElement = <HTMLElement>eleme.nextElementSibling;
-		eleme.classList.toggle('is-active');
-		content!.classList.toggle('is-open');
+		eleme.classList.toggle( 'is-active' );
+		content.classList.toggle( 'is-open' );
 	}
 }
