45f512164eb38524038da50d16988498bedda775
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2021-06-09 16:29:56 +0900

    define  accordion modules

---
+++ src/modules/accordion.ts
@@ -0,0 +1,7 @@
+export function toggle (eleme: HTMLElement)  {
+	if(eleme !== null) {
+		const content: HTMLElement = <HTMLElement>eleme.nextElementSibling;
+		eleme.classList.toggle('is-active');
+		content!.classList.toggle('is-open');
+	}
+}
