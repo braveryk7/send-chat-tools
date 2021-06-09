a7cfc466b5852a21ce4210f04808e69003069039
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2021-06-10 01:17:52 +0900

    lint

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import '../src/scss/style.scss';
 import { toggle } from './modules/accordion';
 
-const title: NodeListOf<HTMLElement> = document.querySelectorAll<HTMLElement>('.accordion-title');
+const title: NodeListOf< HTMLElement > = document.querySelectorAll< HTMLElement >(
+	'.accordion-title'
+);
 
-for(let i: number = 0; i < title.length; i = ( i + 1 )) {
-	title[i]!.addEventListener('click', () => {
-		toggle(title[i]!)
-		console.log(1266549841)
-	});
+for ( let i = 0; i < title.length; i = i + 1 ) {
+	title[ i ]!.addEventListener( 'click', () => {
+		toggle( title[ i ]! );
+	} );
 }
