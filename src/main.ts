b6a9918873ecea4830d1e69af945c025e06ba338
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2021-06-09 16:30:23 +0900

    use accordion modules

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,10 @@
 import '../src/sass/style.scss';
+import { toggle } from './modules/accordion';
+
+const title = document.querySelectorAll<HTMLElement>('.accordion-title');
+
+for(let i: number = 0; i < title.length; i = ( i + 1 )) {
+	title[i].addEventListener('click', () => {
+		toggle(title[i])
+	});
+}
