19da6a12ef2dd451b500c8ca9667a1de2ec239ec
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2021-06-13 15:44:07 +0900

    add TABLE_NAME

---
+++ class/class-sct-const-data.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Admin settings page.
+ *
+ * @author Ken-chan
+ * @package WordPress
+ * @subpackage Send Chat Tools
+ * @since 1.0.0
+ */
+
+declare( strict_type = 1 );
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit( 'You do not have access rights.' );
+}
+
+/**
+ * Constant data.
+ */
+class Sct_Const_Data {
+	/**
+	 * Database Table prefix.
+	 */
+	const TABLE_NAME = 'sct';
+}
