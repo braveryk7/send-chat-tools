7cf6c8efb940339b504271da89f01f8ea19ed2d4
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2021-06-13 21:32:43 +0900

    add constructor, search_table, create_table and insert_log

--- class/class-sct-connect-database.php
+++ class/class-sct-connect-database.php
@@ -19,6 +19,69 @@ if ( ! defined( 'ABSPATH' ) ) {
  */
 class Sct_Connect_Database {
 	/**
+	 * Constructor.
+	 * Gave prefix.
+	 */
+	public function __construct() {
+		global $wpdb;
+		$this->table_name = $wpdb->prefix . Sct_Const_Data::TABLE_NAME;
+	}
+
+	/**
+	 * Search tables.
+	 */
+	public function search_table() {
+		global $wpdb;
+		$get_table = $wpdb->get_var( $wpdb->prepare( 'SHOW TABLES LIKE %s', $this->table_name ) ); // db call ok; no-cache ok.
+		if ( null === $get_table ) {
+			$this->create_table();
+		}
+	}
+
+	/**
+	 * Create table.
+	 */
+	private function create_table() {
+		global $wpdb;
+		$charset_collate = $wpdb->get_charset_collate();
+
+		$sql = "CREATE TABLE $this->table_name (
+			id int unique NOT NULL AUTO_INCREMENT,
+			states smallint(4) UNSIGNED NOT NULL,
+			tool smallint(1) UNSIGNED NOT NULL,
+			type varchar(255) NOT NULL,
+			send_date datetime
+		) $charset_collate;";
+
+		require_once ABSPATH . 'wp-admin/includes/upgrade.php';
+		dbDelta( $sql );
+
+		update_option( 'sct_db_version', Sct_Const_Data::DB_VERSION );
+	}
+
+	/**
+	 * Insert log.
+	 *
+	 * @param string $states_code States code.
+	 * @param string $tool Use tool number.
+	 * @param string $type Type.
+	 */
+	public function insert_log( string $states_code, string $tool, string $type ) {
+		global $wpdb;
+
+		$wpdb->insert(
+			$this->table_name,
+			[
+				'states'    => $states_code,
+				'tool'      => $tool,
+				'type'      => $type,
+				'send_date' => current_time( 'mysql' ),
+			]
+		); // db call ok.
+		update_option( 'sct_logger', 3 );
+	}
+
+	/**
 	 * Delete wp_option.
 	 */
 	public static function delete_db() {
