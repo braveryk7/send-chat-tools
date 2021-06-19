f24d884ce4d2f98018f18b5979eec3420558a377
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2021-06-19 21:08:51 +0900

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,56 +1,76 @@
 const path = require( 'path' );
-const MiniCssExtractPlugin = require( 'mini-css-extract-plugin' );
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
-	entry: './src/main.ts',
-	output: {
-		filename: 'main.js',
-		path: path.resolve( __dirname, 'dist' ),
-	},
-	module: {
-		rules: [
-			{
-				test: /\.ts$/,
-				use: 'ts-loader',
-			},
-			{
-				test: /\.scss$/,
-				use: [
-					{
-						loader: MiniCssExtractPlugin.loader,
-					},
-					{
-						loader: 'css-loader',
-						options: {
-							url: false,
-							sourceMap: true,
-						},
-					},
-					{
-						loader: 'sass-loader',
-						options: {
-							implementation: require( 'sass' ),
-							sassOptions: {
-								fiber: require( 'fibers' ),
-							},
-							sourceMap: true,
-						},
-					},
-				],
-			},
-		],
-	},
+    mode: "development",
 
-	plugins: [
-		new MiniCssExtractPlugin( {
-			filename: 'css/style.css',
-		} ),
-	],
-	devtool: 'source-map',
-	watchOptions: {
-		ignored: /node_modules/,
-	},
-	resolve: {
-		extensions: [ '.ts', '.js' ],
-	},
-};
+    entry: './src/main.ts',
+
+    output: {
+        path: path.resolve( __dirname, 'dist' ),
+        publicPath: '',
+        filename: 'main.js',
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: 'ts-loader',
+            },
+            {
+                test: /\.(scss|sass|css)$/i,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                    },
+                    {
+                        loader: "css-loader",
+                        options: {
+                            url: true,
+                            sourceMap: true,
+                        },
+                    },
+                    {
+                        loader: "sass-loader",
+                        options: {
+                            implementation: require('sass'),
+                            sassOptions: {
+                                fiber: require('fibers'),
+                            },
+                            sourceMap: true,
+                        },
+                    },
+                ],
+            },
+            {
+                test: /\.(gif|png|jpe?g|svg)$/,
+                type: "asset/inline",
+            }
+        ],
+    },
+
+    resolve: {
+        extensions: [
+            '.ts', '.js'
+        ],
+    },
+
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "css/style.css",
+        }),
+        new CleanWebpackPlugin({
+            cleanStaleWebpackAssets: false,
+        }),
+    ],
+
+    devtool: "source-map",
+
+    watchOptions: {
+        ignored: /node_modules/
+    },
+
+    target: ["web", "es5"],
+}
