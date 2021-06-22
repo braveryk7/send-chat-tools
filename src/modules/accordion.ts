f905ad42ab8bc66a35557d49f30a9500de4753bf
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2021-06-22 16:54:03 +0900

    Classified

--- src/modules/accordion.ts
+++ src/modules/accordion.ts
@@ -1,7 +1,25 @@
-export function toggle( eleme: HTMLElement ) {
-	if ( eleme !== null ) {
-		const content: HTMLElement = <HTMLElement>eleme.nextElementSibling;
-		eleme.classList.toggle( 'is-active' );
-		content.classList.toggle( 'is-open' );
+export class SctAccordion {
+	private title: NodeListOf< HTMLElement >;
+
+	constructor() {
+		this.title = document.querySelectorAll< HTMLElement >(
+			'.accordion-title'
+		);
+
+		for ( let i = 0; i < this.title.length; i = i + 1 ) {
+			this.title[ i ]!.addEventListener( 'click', () => {
+				this.toggle( this.title[ i ]! );
+			} );
+		}
+	}
+
+	toggle( eleme: HTMLElement ) {
+		if ( eleme !== null ) {
+			const content: HTMLElement = <HTMLElement>eleme.nextElementSibling;
+			eleme.classList.toggle( 'is-active' );
+			content.classList.toggle( 'is-open' );
+		}
 	}
 }
+
+new SctAccordion();
