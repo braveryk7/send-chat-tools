0bc2ab95392fe4cd14cd64a46757e9c9c616c4cb
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2021-11-07 15:32:59 +0900

    refs #44 delete

--- src/main.ts
+++
@@ -1,9 +0,0 @@
-import '../src/scss/style.scss';
-import { SctAccordion } from './modules/_SctAccordion';
-import { SctApiCheck } from './modules/_SctApiCheck';
-
-// Run Accordion.
-new SctAccordion();
-
-// Run Api check.
-new SctApiCheck();
