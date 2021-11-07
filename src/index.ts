ff3680b0b8f92649d74cb84fb4eb214b8c82a334
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2021-11-07 15:32:34 +0900

    refs #44 Rename main.ts -> index.ts

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import './scss/style.scss';
+import { SctAccordion } from './modules/_SctAccordion';
+import { SctApiCheck } from './modules/_SctApiCheck';
+
+// Run Accordion.
+new SctAccordion();
+
+// Run Api check.
+new SctApiCheck();
