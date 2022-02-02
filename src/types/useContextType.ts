fc0c7a5fd4c305b24efb2b3f815150ca1d045dbc
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2022-02-02 15:00:33 +0900

    refs #46 add noticeValueType, apiContextType

---
+++ src/types/useContextType.ts
@@ -0,0 +1,14 @@
+import { Dispatch, SetStateAction } from 'react';
+
+import { apiType } from './apiType';
+
+export type noticeValueType = 'sct_success' | 'sct_error' | undefined;
+
+export type apiContextType = {
+	apiData: apiType;
+	setApiData: Dispatch< SetStateAction< apiType > >;
+	setNoticeStatus: Dispatch< SetStateAction< boolean > >;
+	setNoticeValue: Dispatch< SetStateAction< noticeValueType > >;
+	setNoticeMessage: Dispatch< SetStateAction< string > >;
+	snackbarTimer: NodeJS.Timeout;
+};
