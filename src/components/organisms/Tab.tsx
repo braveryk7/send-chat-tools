5812334f6377010f3fdbf8dd156c2396f1507520
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2022-02-03 00:16:57 +0900

    refs #46 add Tab components

---
+++ src/components/organisms/Tab.tsx
@@ -0,0 +1,37 @@
+import { TabPanel } from '@wordpress/components';
+import { __ } from '@wordpress/i18n';
+
+import { Items } from '../molecules/TabItems';
+
+export const Tab = () => {
+	return (
+		<TabPanel
+			activeClass="active-tab"
+			className="settings-tab"
+			tabs={ [
+				{
+					name: 'basic',
+					title: __( 'Basic', 'send-chat-tools' ),
+					className: 'tab-basic',
+				},
+				{
+					name: 'slack',
+					title: 'Slack',
+					className: 'tab-slack',
+				},
+				{
+					name: 'discord',
+					title: 'Discord',
+					className: 'tab-discord',
+				},
+				{
+					name: 'chatwork',
+					title: 'Chatwork',
+					className: 'tab-chatwork',
+				},
+			] }
+		>
+			{ ( tab ) => <Items id={ tab.name } title={ tab.title } /> }
+		</TabPanel>
+	);
+};
