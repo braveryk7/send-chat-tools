3278bee08cba42f584afaca84fb67c2fb916c24c
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2022-02-02 20:23:55 +0900

    refs #46 add types

--- src/types/apiType.ts
+++ src/types/apiType.ts
@@ -1,7 +1,13 @@
 export type apiType = {
 	sct_use_slack?: boolean, // eslint-disable-line
+	sct_send_slack_author?: boolean, // eslint-disable-line
+	sct_send_slack_update?: boolean, // eslint-disable-line
 	sct_use_discord?: boolean, // eslint-disable-line
+	sct_send_discord_author?: boolean, // eslint-disable-line
+	sct_send_discord_update?: boolean, // eslint-disable-line
 	sct_use_chatwork?: boolean, // eslint-disable-line
+	sct_send_chatwork_author?: boolean, // eslint-disable-line
+	sct_send_chatwork_update?: boolean, // eslint-disable-line
 };
 
 export type WPApiType< T > = {
