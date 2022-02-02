5a506341b23b54fe2796f8484aa74b703971ee1b
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2022-02-03 00:28:13 +0900

    refs #46 add itemKey

--- src/types/ComponentsType.ts
+++ src/types/ComponentsType.ts
@@ -1,4 +1,15 @@
+export type itemKey =
+	| 'sct_use_slack'
+	| 'sct_send_slack_author'
+	| 'sct_send_slack_update'
+	| 'sct_use_discord'
+	| 'sct_send_discord_author'
+	| 'sct_send_discord_update'
+	| 'sct_use_chatwork'
+	| 'sct_send_chatwork_author'
+	| 'sct_send_chatwork_update';
+
 export type TogglePropsType = {
-	itemKey: string;
+	itemKey: itemKey;
 	label: string;
 };
