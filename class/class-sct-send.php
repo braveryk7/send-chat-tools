dc94d1acf135e3040654d23a27871036efc533d8
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2022-02-03 22:43:17 +0900

    refs #55 add class base

--- class/class-sct-send.php
+++ class/class-sct-send.php
@@ -1,1 +1,21 @@
+<?php
+/**
+ * Use Slack.
+ *
+ * @author Ken-chan
+ * @package WordPress
+ * @subpackage Send Chat Tools
+ * @since 1.3.0
+ */
 
+declare( strict_types = 1 );
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit( 'You do not have access rights.' );
+}
+
+/**
+ * Send tools & email.
+ */
+class Sct_Send {
+}
