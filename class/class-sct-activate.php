c6c5400d5b7e3aeb2ca32616390183d89568c38f
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2022-02-04 00:37:42 +0900

    refs #56 add register_options method

--- class/class-sct-activate.php
+++ class/class-sct-activate.php
@@ -18,4 +18,38 @@ if ( ! defined( 'ABSPATH' ) ) {
  * Activate process.
  */
 class Sct_Activate extends Sct_Base {
+	/**
+	 * Register wp_options column.
+	 */
+	public function register_options() {
+		$chat_tools_value = [
+			'use'         => false,
+			'webhook_url' => '',
+			'send_author' => false,
+			'send_update' => false,
+			'log'         => '',
+		];
+		$chatwork_value   = [
+			'use'         => false,
+			'api_token'   => '',
+			'room_id'     => '',
+			'send_author' => false,
+			'send_update' => false,
+			'log'         => '',
+		];
+
+		$iv = Sct_Encryption::make_vector();
+
+		$options = [
+			'slack'      => [ $chat_tools_value ],
+			'discord'    => [ $chat_tools_value ],
+			'chatwork'   => [ $chatwork_value ],
+			'db_version' => self::DB_VERSION,
+			'iv'         => $iv,
+			'user_id'    => '',
+			'cron_time'  => false,
+		];
+
+		add_option( $this->add_prefix( 'options' ), $options );
+	}
 }
