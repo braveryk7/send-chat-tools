514c36f30781d81a2d0c3ffb6b728c13c95250d0
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2022-02-04 00:09:50 +0900

    refs #56 add class base

---
+++ class/class-sct-activate.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Plugin activate.
+ *
+ * @author Ken-chan
+ * @package WordPress
+ * @subpackage Send Chat Tools
+ * @since 1.3.0
+ */
+
+declare( strict_types = 1 );
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit( 'You do not have access rights.' );
+}
+
+/**
+ * Activate process.
+ */
+class Sct_Activate extends Sct_Base {
+}
