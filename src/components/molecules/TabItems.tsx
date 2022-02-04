e35103abc98b256b3732ce75640f82c4c8f3f1ef
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2022-02-05 08:23:31 +0900

    refs #32 fix logic
    delete authorItemKey, updateItemKey
    fix component view terms use itemKey: not null
    add optionName
    add itemKeyType

--- src/components/molecules/TabItems.tsx
+++ src/components/molecules/TabItems.tsx
@@ -1,41 +1,29 @@
 import { useEffect, useState } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 
-import { itemKey } from '../../types/ComponentsType';
+import { itemKeyType } from '../../types/ComponentsType';
 import { Toggle } from '../atoms/Toggle';
 
 export const Items = ( props: any ) => {
 	const { id, title } = props;
 	const [ basicFlag, setBasicFlag ] = useState( false );
-	const [ itemKeys, setItemKeys ] = useState< itemKey >( 'sct_use_slack' );
-	const [ authorItemKey, setAuthorItemKey ] = useState< itemKey >(
-		'sct_send_slack_author'
-	);
-	const [ updateItemKey, setUpdateItemKey ] = useState< itemKey >(
-		'sct_send_slack_update'
-	);
+	const [ itemKey, setItemKey ] = useState< itemKeyType | null >( null );
 	const [ itemLabel, setItemLabel ] = useState( '' );
 
 	useEffect( () => {
 		switch ( id ) {
 			case 'slack':
-				setItemKeys( 'sct_use_slack' );
-				setAuthorItemKey( 'sct_send_slack_author' );
-				setUpdateItemKey( 'sct_send_slack_update' );
+				setItemKey( 'slack' );
 				setItemLabel( __( 'Slack通知を使用する', 'send-chat-tools' ) );
 				break;
 			case 'discord':
-				setItemKeys( 'sct_use_discord' );
-				setAuthorItemKey( 'sct_send_discord_author' );
-				setUpdateItemKey( 'sct_send_discord_update' );
+				setItemKey( 'discord' );
 				setItemLabel(
 					__( 'Discord通知を使用する', 'send-chat-tools' )
 				);
 				break;
 			case 'chatwork':
-				setItemKeys( 'sct_use_chatwork' );
-				setAuthorItemKey( 'sct_send_chatwork_author' );
-				setUpdateItemKey( 'sct_send_chatwork_update' );
+				setItemKey( 'chatwork' );
 				setItemLabel(
 					__( 'Chatwork通知を使用する', 'send-chat-tools' )
 				);
@@ -49,19 +37,27 @@ export const Items = ( props: any ) => {
 			<h2>
 				{ title } { __( 'settings', 'send-chat-tools' ) }
 			</h2>
-			<Toggle itemKey={ itemKeys } label={ itemLabel } />
-			{ basicFlag && (
+			{ itemKey && (
 				<Toggle
-					itemKey={ authorItemKey }
+					itemKey={ itemKey }
+					optionName="use"
+					label={ itemLabel }
+				/>
+			) }
+			{ basicFlag && itemKey && (
+				<Toggle
+					itemKey={ itemKey }
+					optionName="send_author"
 					label={ __(
 						'自分自身のコメントを送信しない',
 						'send-chat-tools'
 					) }
 				/>
 			) }
-			{ basicFlag && (
+			{ basicFlag && itemKey && (
 				<Toggle
-					itemKey={ updateItemKey }
+					itemKey={ itemKey }
+					optionName="send_update"
 					label={ __(
 						'アップデート通知を使用する',
 						'send-chat-tools'
