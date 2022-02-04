280e1c51318e85adc6fae6f55204d3d1de26097e
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2022-02-05 08:17:57 +0900

    refs #32 add itemKeyType, optionNameType
    fit TogglePropsType use itemKeyType, optionNameType

--- src/types/ComponentsType.ts
+++ src/types/ComponentsType.ts
@@ -1,15 +1,9 @@
-export type itemKey =
-	| 'sct_use_slack'
-	| 'sct_send_slack_author'
-	| 'sct_send_slack_update'
-	| 'sct_use_discord'
-	| 'sct_send_discord_author'
-	| 'sct_send_discord_update'
-	| 'sct_use_chatwork'
-	| 'sct_send_chatwork_author'
-	| 'sct_send_chatwork_update';
+export type itemKeyType = 'slack' | 'discord' | 'chatwork' | 'basic';
+
+export type optionNameType = 'use' | 'send_author' | 'send_update';
 
 export type TogglePropsType = {
-	itemKey: itemKey;
+	itemKey: itemKeyType;
+	optionName: optionNameType;
 	label: string;
 };
