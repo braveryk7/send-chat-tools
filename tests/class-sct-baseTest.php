a99e3c48f57bc27c2fbf35e2adffd40db7122a0b
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2022-02-05 18:40:22 +0900

    refs #73 feat test class base

--- tests/class-sct-baseTest.php
+++ tests/class-sct-baseTest.php
@@ -1,1 +1,12 @@
+<?php
+declare( strict_types = 1 );
 
+define( 'ABSPATH', '' );
+
+require_once './class/class-sct-base.php';
+
+/**
+ * Test: Sct_Base
+ */
+class Sct_BaseTest extends PHPUnit\Framework\TestCase {
+}
