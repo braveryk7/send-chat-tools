3066fd0b937612230e833924e8ec67738f1770bb
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2022-02-05 15:20:26 +0900

    refs #32 add TextControlPropsType
    fix optionNameType

--- src/types/ComponentsType.ts
+++ src/types/ComponentsType.ts
@@ -1,9 +1,17 @@
 export type itemKeyType = 'slack' | 'discord' | 'chatwork' | 'basic';
 
-export type optionNameType = 'use' | 'send_author' | 'send_update';
+export type optionNameType =
+	| 'use'
+	| 'send_author'
+	| 'send_update'
+	| 'webhook_url'
+	| 'api_token'
+	| 'room_id';
 
 export type TogglePropsType = {
 	itemKey: itemKeyType;
 	optionName: optionNameType;
 	label: string;
 };
+
+export type TextControlPropsType = TogglePropsType;
