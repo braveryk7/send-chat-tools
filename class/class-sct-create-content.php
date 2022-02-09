a49cef95c33c963c563c94a23b7e9a4be0876b74
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2022-02-09 22:19:36 +0900

    refs #78 delete get_send_author

--- class/class-sct-create-content.php
+++ class/class-sct-create-content.php
@@ -92,20 +92,6 @@ class Sct_Create_Content extends Sct_Base {
 	}
 
 	/**
-	 * Get send_author status.
-	 *
-	 * @param boolean $author_status sct_options->TOOLNAME->use.
-	 * @param string  $comment_user_id Comment user ID.
-	 */
-	private function get_send_author( $author_status, $comment_user_id ) {
-		if ( ! $author_status || $author_status && '0' === $comment_user_id ) {
-			return true;
-		} else {
-			return false;
-		}
-	}
-
-	/**
 	 * Check send status.
 	 *
 	 * @param string $tool_name Tool name.
