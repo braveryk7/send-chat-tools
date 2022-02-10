e5d19ef03ec5488ed152a3f5e7b725b3b114cb79
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2022-02-10 16:26:40 +0900

    refs #87 add crypto2plain method

--- class/class-sct-activate.php
+++ class/class-sct-activate.php
@@ -140,6 +140,8 @@ class Sct_Activate extends Sct_Base {
 				}
 			}
 			update_option( $this->add_prefix( 'options' ), $sct_options );
+
+			$this->crypto2plain( $sct_options );
 		}
 
 		/**
@@ -171,5 +173,34 @@ class Sct_Activate extends Sct_Base {
 				update_option( $this->add_prefix( 'logs' ), $sct_logs );
 			}
 		}
+	}
+
+	/**
+	 * Encrypt -> Plain.
+	 *
+	 * @param array $sct_options Send Chat Tools options.
+	 */
+	private function crypto2plain( array $sct_options ) {
+		$tools = [ 'slack', 'discord', 'chatwork' ];
+
+		$encryption = new Sct_Encryption();
+
+		foreach ( $tools as $tool ) {
+			'chatwork' !== $tool ? $api_key = 'webhook_url' : $api_key = 'api_token';
+			$api_value                      = $encryption->decrypt( $sct_options[ $tool ][ $api_key ] );
+
+			if ( $api_value ) {
+				$sct_options[ $tool ][ $api_key ] = $api_value;
+			}
+
+			if ( 'chatwork' === $tool ) {
+				$this->console( $sct_options[ $tool ]['room_id'] );
+				$room_id = $encryption->decrypt( $sct_options[ $tool ]['room_id'] );
+				if ( $room_id ) {
+					$sct_options[ $tool ]['room_id'] = $room_id;
+				}
+			}
+		}
+		$this->set_sct_options( $sct_options );
 	}
 }
