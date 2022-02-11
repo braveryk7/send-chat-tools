f1e1b73427d720f76046b228cfd18d6515375168
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2022-02-11 12:13:16 +0900

    refs #93 delete basic & refactor

--- src/components/organisms/Tab.tsx
+++ src/components/organisms/Tab.tsx
@@ -4,37 +4,33 @@ import { __ } from '@wordpress/i18n';
 import { Items } from '../molecules/TabItems';
 
 export const Tab = () => {
+	const tabs = [
+		{
+			name: 'slack',
+			title: 'Slack',
+			className: 'tab-slack',
+		},
+		{
+			name: 'discord',
+			title: 'Discord',
+			className: 'tab-discord',
+		},
+		{
+			name: 'chatwork',
+			title: 'Chatwork',
+			className: 'tab-chatwork',
+		},
+		{
+			name: 'logs',
+			title: 'Logs',
+			className: 'tab-logs',
+		},
+	];
 	return (
 		<TabPanel
 			activeClass="active-tab"
 			className="settings-tab"
-			tabs={ [
-				{
-					name: 'basic',
-					title: __( 'Basic', 'send-chat-tools' ),
-					className: 'tab-basic',
-				},
-				{
-					name: 'slack',
-					title: 'Slack',
-					className: 'tab-slack',
-				},
-				{
-					name: 'discord',
-					title: 'Discord',
-					className: 'tab-discord',
-				},
-				{
-					name: 'chatwork',
-					title: 'Chatwork',
-					className: 'tab-chatwork',
-				},
-				{
-					name: 'logs',
-					title: 'Logs',
-					className: 'tab-logs',
-				},
-			] }
+			tabs={ tabs }
 		>
 			{ ( tab ) => <Items id={ tab.name } title={ tab.title } /> }
 		</TabPanel>
