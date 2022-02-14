87c1383761ac7c4794ac344bb302ec3bc51ef25e
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2022-02-14 22:13:34 +0900

    refs #129 feat logic TabItems -> useTabItems

--- src/components/molecules/TabItems.tsx
+++ src/components/molecules/TabItems.tsx
@@ -1,12 +1,7 @@
-import { useEffect, useState } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 
-import {
-	itemKeyType,
-	optionNameType,
-	TabItemsType,
-	TogglePropsType,
-} from '../../types/ComponentsType';
+import { useTabItems } from '../../hooks/useTabItems';
+import { TabItemsType } from '../../types/ComponentsType';
 import { LogDetail } from '../atoms/LogDetail';
 import { LogView } from '../atoms/LogView';
 import { TextControlForm } from '../atoms/TextControlForm';
@@ -14,83 +9,17 @@ import { Toggle } from '../atoms/Toggle';
 import { UpdateTime } from '../atoms/UpdateTime';
 
 export const Items = ( props: TabItemsType ) => {
-	const { id } = props;
-	const [ updateFlag, setUpdateFlag ] = useState( false );
-	const [ logsFlag, setLogsFlag ] = useState( false );
-	const [ tabItems, setTabItems ] = useState< TogglePropsType[] >( [] );
-	const [ itemKey, setItemKey ] = useState< itemKeyType | null >( null );
-	const [ itemLabel, setItemLabel ] = useState( '' );
-	const [
-		apiOptionName,
-		setApiOptionName,
-	] = useState< optionNameType | null >( null );
-	const [ titleText, setTitleText ] = useState( '' );
-	const [ textLabel, setTextLabel ] = useState( '' );
-	const [
+	const {
+		updateFlag,
+		itemKey,
+		logsFlag,
+		titleText,
+		textLabel,
+		tabItems,
 		chatworkRoomId,
-		setChatworkRoomId,
-	] = useState< optionNameType | null >( null );
-	const [ chatworkText, setChatworkText ] = useState( '' );
-
-	useEffect( () => {
-		switch ( id ) {
-			case 'slack':
-				setItemKey( 'slack' );
-				setItemLabel( __( 'Use Slack notify', 'send-chat-tools' ) );
-				setApiOptionName( 'webhook_url' );
-				setTitleText( __( 'Slack settings', 'send-chat-tools' ) );
-				setTextLabel( __( 'Slack Webhook URL', 'send-chat-tools' ) );
-				break;
-			case 'discord':
-				setItemKey( 'discord' );
-				setItemLabel( __( 'Use Discord notify', 'send-chat-tools' ) );
-				setApiOptionName( 'webhook_url' );
-				setTitleText( __( 'Discord settings', 'send-chat-tools' ) );
-				setTextLabel( __( 'Discord Webhook URL', 'send-chat-tools' ) );
-				break;
-			case 'chatwork':
-				setItemKey( 'chatwork' );
-				setItemLabel( __( 'Use Chatwork notify', 'send-chat-tools' ) );
-				setApiOptionName( 'api_token' );
-				setChatworkRoomId( 'room_id' );
-				setTitleText( __( 'Chatwork settings', 'send-chat-tools' ) );
-				setTextLabel( __( 'Chatwork API token', 'send-chat-tools' ) );
-				setChatworkText( __( 'Chatwork Room ID', 'send-chat-tools' ) );
-				break;
-			case 'update':
-				setUpdateFlag( true );
-				setTitleText(
-					__( 'Update notify settings', 'send-chat-tools' )
-				);
-				break;
-			case 'logs':
-				setLogsFlag( true );
-				setTitleText( __( 'Logs', 'send-chat-tools' ) );
-				break;
-		}
-	}, [ id ] );
-
-	useEffect( () => {
-		if ( itemKey ) {
-			const items: TogglePropsType[] = [
-				{ itemKey, optionName: 'use', label: itemLabel },
-				{
-					itemKey,
-					optionName: 'send_author',
-					label: __(
-						"Don't send your own comments",
-						'send-chat-tools'
-					),
-				},
-				{
-					itemKey,
-					optionName: 'send_update',
-					label: __( 'Use Update notify', 'send-chat-tools' ),
-				},
-			];
-			setTabItems( items );
-		}
-	}, [ itemKey ] );
+		chatworkText,
+		apiOptionName,
+	} = useTabItems( props );
 
 	return (
 		<div className="sct-wrapper">
