fc40ef8070cfabe79681d812d479c7b003deff0d
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2022-02-16 00:18:27 +0900

    refs #156 add class base code

--- class/class-sct-dashboard-notify.php
+++ class/class-sct-dashboard-notify.php
@@ -1,1 +1,21 @@
+<?php
+/**
+ * Plugin notify
+ *
+ * @author Ken-chan
+ * @package WordPress
+ * @subpackage Send Chat Tools
+ * @since 1.3.0
+ */
 
+declare( strict_types = 1 );
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit( 'You do not have access rights.' );
+}
+
+/**
+ * Plugin notify on WordPress dashboard.
+ */
+class Sct_Dashboard_Notify {
+}
