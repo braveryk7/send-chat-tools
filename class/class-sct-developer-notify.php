5c071c0c2c63ad7858d0d4100d12655c79875d9b
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2022-02-19 23:51:51 +0900

    refs #162 add developer_message_arraykeys_check method

--- class/class-sct-developer-notify.php
+++ class/class-sct-developer-notify.php
@@ -18,5 +18,20 @@ if ( ! defined( 'ABSPATH' ) ) {
  * Developer notify class.
  */
 class Sct_Developer_Notify extends Sct_Base {
+	/**
+	 * Check for the presence of keys in developer messages.
+	 *
+	 * @param array $developer_message Developer message.
+	 */
+	private function developer_message_arraykeys_check( array $developer_message ): bool {
+		$flag = false;
 
+		$flag = array_key_exists( 'key', $developer_message ) ? true : false;
+		$flag = array_key_exists( 'type', $developer_message ) ? true : false;
+		$flag = array_key_exists( 'title', $developer_message ) ? true : false;
+		$flag = array_key_exists( 'message', $developer_message ) ? true : false;
+		$flag = array_key_exists( 'url', $developer_message ) ? true : false;
+
+		return $flag;
+	}
 }
