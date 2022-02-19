7c0979e693fb5a99c1f40df45f858c577bb7589f
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2022-02-19 21:55:36 +0900

    refs #162 add class base

---
+++ class/class-sct-developer-notify.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Developer notify.
+ *
+ * @author Ken-chan
+ * @package WordPress
+ * @subpackage Send Chat Tools
+ * @since 1.3.0
+ */
+
+declare( strict_types = 1 );
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit( 'You do not have access rights.' );
+}
+
+/**
+ * Developer notify class.
+ */
+class Sct_Developer_Notify extends Sct_Base {
+
+}
