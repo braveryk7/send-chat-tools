45e2cc5f07c0104301d89c14893de2744925f761
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2022-02-22 00:07:20 +0900

    refs #201 add test_developer_message_arraykeys_check method

--- tests/class-sct-developer-notifyTest.php
+++ tests/class-sct-developer-notifyTest.php
@@ -39,6 +39,13 @@ class Sct_Developer_NotifyTest extends PHPUnit\Framework\TestCase {
 	}
 
 	/**
+	 * TEST: developer_message_arraykeys_check()
+	 */
+	public function test_developer_message_arraykeys_check() {
+		$this->markTestIncomplete( 'This test is incomplete.' );
+	}
+
+	/**
 	 * TEST: developer_message_urls_regex()
 	 *
 	 * @dataProvider url_parameters
