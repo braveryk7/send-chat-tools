3a9a2ac69c9970fbdd3232c656199e15f976720e
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2022-02-21 23:01:22 +0900

    refs #201 add test base class

---
+++ tests/class-sct-developer-notifyTest.php
@@ -0,0 +1,13 @@
+<?php
+declare( strict_types = 1 );
+
+define( 'ABSPATH', '' );
+
+require_once './class/class-sct-developer-notify.php';
+
+/**
+ * Test: Sct_Developer_Notify
+ */
+class Sct_Developer_NotifyTest extends PHPUnit\Framework\TestCase {
+
+}
