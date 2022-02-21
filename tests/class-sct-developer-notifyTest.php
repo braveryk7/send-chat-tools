7846366879634a26cc660fc2141ca1315cf45886
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2022-02-22 00:06:13 +0900

    refs #201 add test_developer_message_controller method

--- tests/class-sct-developer-notifyTest.php
+++ tests/class-sct-developer-notifyTest.php
@@ -32,6 +32,13 @@ class Sct_Developer_NotifyTest extends PHPUnit\Framework\TestCase {
 	}
 
 	/**
+	 * TEST: developer_message_controller()
+	 */
+	public function test_developer_message_controller() {
+		$this->markTestIncomplete( 'This test is incomplete.' );
+	}
+
+	/**
 	 * TEST: developer_message_urls_regex()
 	 *
 	 * @dataProvider url_parameters
