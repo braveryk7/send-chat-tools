1fafd2c6e968d7a3dfd0da6718a7a1ebd0d95abb
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2022-02-27 14:10:50 +0900

    refs #214 add instance propertie

--- tests/SctCheckUpdateTest.php
+++ tests/SctCheckUpdateTest.php
@@ -5,5 +5,10 @@ declare( strict_types = 1 );
  * Test: Sct_Admin_Page
  */
 class SctCheckUpdateTest extends PHPUnit\Framework\TestCase {
-
+	/**
+	 * This test class instance.
+	 *
+	 * @var object $instance instance.
+	 */
+	private $instance;
 }
