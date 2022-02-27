9982304bcb322b326a13ed19b428459ef48bf831
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2022-02-27 14:33:02 +0900

    refs #214 add instance propety

--- tests/SctErrorMailTest.php
+++ tests/SctErrorMailTest.php
@@ -5,4 +5,10 @@ declare( strict_types = 1 );
  * Test: Sct_Error_Mail
  */
 class SctErrorMailTest extends PHPUnit\Framework\TestCase {
+	/**
+	 * This test class instance.
+	 *
+	 * @var object $instance instance.
+	 */
+	private $instance;
 }
