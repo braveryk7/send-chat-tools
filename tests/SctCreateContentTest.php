bdff13c240a1b4680f70f44b83e648b8f885b8ab
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2022-02-27 14:16:09 +0900

    refs #214 add instance propertie

--- tests/SctCreateContentTest.php
+++ tests/SctCreateContentTest.php
@@ -5,4 +5,10 @@ declare( strict_types = 1 );
  * Test: Sct_Create_Content
  */
 class SctCreateContentTest extends PHPUnit\Framework\TestCase {
+	/**
+	 * This test class instance.
+	 *
+	 * @var object $instance instance.
+	 */
+	private $instance;
 }
