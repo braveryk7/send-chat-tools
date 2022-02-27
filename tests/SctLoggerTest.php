5c12dd64faa74ca46e474590a5e5b3dc635bbe63
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2022-02-27 14:39:13 +0900

    refs #214 add instance propety

--- tests/SctLoggerTest.php
+++ tests/SctLoggerTest.php
@@ -5,4 +5,10 @@ declare( strict_types = 1 );
  * Test: Sct_Logger
  */
 class SctLoggerTest extends PHPUnit\Framework\TestCase {
+	/**
+	 * This test class instance.
+	 *
+	 * @var object $instance instance.
+	 */
+	private $instance;
 }
