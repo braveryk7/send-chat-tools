a54c0930483f23d1888c2a998d274749433fb9d2
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2022-02-27 13:28:39 +0900

    refs #214 add instance propertie

--- tests/class-sct-activateTest.php
+++ tests/class-sct-activateTest.php
@@ -5,5 +5,10 @@ declare( strict_types = 1 );
  * Test: Sct_Activate
  */
 class Sct_ActivateTest extends PHPUnit\Framework\TestCase {
-
+	/**
+	 * This test class instance.
+	 *
+	 * @var object $instance instance.
+	 */
+	private $instance;
 }
