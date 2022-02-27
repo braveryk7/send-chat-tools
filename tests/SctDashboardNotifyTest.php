1bdfb8e5aee91f7dff6576856efd01f9c5d0e9e9
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2022-02-27 14:23:55 +0900

    refs #214 add instance propertie

--- tests/SctDashboardNotifyTest.php
+++ tests/SctDashboardNotifyTest.php
@@ -5,4 +5,10 @@ declare( strict_types = 1 );
  * Test: Sct_Dashboard_Notify
  */
 class SctDashboardNotifyTest extends PHPUnit\Framework\TestCase {
+	/**
+	 * This test class instance.
+	 *
+	 * @var object $instance instance.
+	 */
+	private $instance;
 }
