14d6f83bc257fd8b57cb3be177534f4073a80754
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2022-02-27 14:42:49 +0900

    refs #214 add setUpBeforeClass method

--- tests/SctSlackBlocksTest.php
+++ tests/SctSlackBlocksTest.php
@@ -11,4 +11,16 @@ class SctSlackBlocksTest extends PHPUnit\Framework\TestCase {
 	 * @var object $instance instance.
 	 */
 	private $instance;
+
+	/**
+	 * Settings: ABSPATH, test class file, WordPress functions.
+	 */
+	public static function setUpBeforeClass(): void {
+		if ( ! defined( 'ABSPATH' ) ) {
+			define( 'ABSPATH', '' );
+		}
+
+		require_once './class/class-sct-slack-blocks.php';
+		require_once './tests/lib/wordpress-functions.php';
+	}
 }
