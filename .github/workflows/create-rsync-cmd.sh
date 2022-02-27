8516120425a22bd2cb10a7459af73a278f5e1151
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2022-02-27 11:49:40 +0900

    refs #210 add process

---
+++ .github/workflows/create-rsync-cmd.sh
@@ -0,0 +1,89 @@
+#!/bin/sh
+
+# SVN NAME
+SVN_NAME="admin-bar-tools"
+
+# Output path
+output_path="${SVN_NAME}/trunk/"
+
+# Exclude files
+# format -> "file or directory name"
+excludes=(
+	${SVN_NAME}
+	".git"
+	".github"
+	".gitignore"
+	".vscode"
+	".cache"
+	".DS_Store"
+	"Thumbs.db"
+	"src"
+	"cmd"
+	"conf"
+	# composer
+	"vendor"
+	"composer.json"
+	"composer.lock"
+	".phpcs.xml"
+	"phpcs.xml"
+	".phpcs.xml.dist"
+	"phpcs.xml.dist"
+	"ruleset.xml"
+	"phpstan.neon"
+	"phpunit.xml"
+	"tests"
+	# npm
+	".npm"
+	"node_modules"
+	"package.json"
+	"package-lock.json"
+	".babelrc"
+	".babelrc.json"
+	".babelrc.js"
+	".babelrc.cjs"
+	".babelrc.mjs"
+	".babel.config.json"
+	".babel.config.js"
+	".babel.config.cjs"
+	".babel.config.mjs"
+	".eslintrc.js"
+	".eslintrc.cjs"
+	".eslintrc.json"
+	".eslintrc.yaml"
+	".eslintrc.yml"
+	".stylelintrc"
+	".stylelintrc.js"
+	".stylelintrc.cjs"
+	".stylelintrc.json"
+	".stylelintrc.yaml"
+	".stylelintrc.yml"
+	"jest.config.js"
+	"jest.config.cjs"
+	"jest.config.mjs"
+	"jest.config.ts"
+	"jest.config.json"
+	"tsconfig.json"
+	"webpack.config.js"
+)
+
+# Delete trunk directory.
+rm ${output_path}
+
+# Create trunk directory.
+mkdir ${output_path}
+
+# Get current file, directory.
+for file in .[^\.]*;
+	do
+		current_file_list+=(${file#./*})
+	done
+
+# Create exclude list.
+cmd_exclude=""
+for exclude in "${excludes[@]}";
+	do
+		cmd_exclude="${cmd_exclude} --exclude '${exclude}'"
+	done
+
+cmd="rsync -a ./ ${output_path} ${cmd_exclude}"
+eval ${cmd}
