490783ece85f8af9367e1bbbd916651fb82166bc
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2022-02-27 13:29:26 +0900

    refs #214 add setUpBeforeClass method

--- tests/class-sct-activateTest.php
+++ tests/class-sct-activateTest.php
@@ -11,4 +11,20 @@ class Sct_ActivateTest extends PHPUnit\Framework\TestCase {
 	 * @var object $instance instance.
 	 */
 	private $instance;
+
+	/**
+	 * Settings: ABSPATH, test class file, WordPress functions.
+	 */
+	public static function setUpBeforeClass(): void {
+		if ( ! defined( 'ABSPATH' ) ) {
+			define( 'ABSPATH', '' );
+		}
+
+		if ( ! class_exists( 'Sct_Base ' ) ) {
+			require_once './class/class-sct-base.php';
+		}
+
+		require_once './class/class-sct-activate.php';
+		require_once './tests/lib/wordpress-functions.php';
+	}
 }
