c27043d5f5f8501da21e2f57e78a5116379ef00f
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2022-02-27 14:20:15 +0900

    refs #214 add test_create_comment_message method

--- tests/SctCreateContentTest.php
+++ tests/SctCreateContentTest.php
@@ -59,4 +59,11 @@ class SctCreateContentTest extends PHPUnit\Framework\TestCase {
 	public function test_create_content() {
 		$this->markTestIncomplete( 'This test is incomplete.' );
 	}
+
+	/**
+	 * TEST: create_comment_message()
+	 */
+	public function test_create_comment_message() {
+		$this->markTestIncomplete( 'This test is incomplete.' );
+	}
 }
