144c3ff3026b4c02d941cd581883094074ba3b94
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2022-03-06 00:31:34 +0900

    refs #256 fix setApiData type

--- src/types/useContextType.ts
+++ src/types/useContextType.ts
@@ -6,7 +6,7 @@ export type noticeValueType = 'sct_success' | 'sct_error' | undefined;
 
 export type apiContextType = {
 	apiData: apiType | null;
-	setApiData: Dispatch< SetStateAction< apiType > >;
+	setApiData: Dispatch< SetStateAction< apiType | null > >;
 	setNoticeStatus: Dispatch< SetStateAction< boolean > >;
 	setNoticeValue: Dispatch< SetStateAction< noticeValueType > >;
 	setNoticeMessage: Dispatch< SetStateAction< string > >;
