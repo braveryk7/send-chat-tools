8da2c828d0f45aa9aded91ea179aaebed56e6c28
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2022-03-06 00:35:00 +0900

    refs #256 delete setNoticeStatus

--- src/types/useContextType.ts
+++ src/types/useContextType.ts
@@ -7,7 +7,6 @@ export type noticeValueType = 'sct_success' | 'sct_error' | null;
 export type apiContextType = {
 	apiData: apiType | null;
 	setApiData: Dispatch< SetStateAction< apiType | null > >;
-	setNoticeStatus: Dispatch< SetStateAction< boolean > >;
 	setNoticeValue: Dispatch< SetStateAction< noticeValueType > >;
 	setNoticeMessage: Dispatch< SetStateAction< string > >;
 	snackbarTimer: number;
