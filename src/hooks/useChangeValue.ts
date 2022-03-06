5f4e0849d09613a0c93bc084bca89881502a27c8
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2022-03-07 02:55:05 +0900

    refs #80 add useChangeValue logic

---
+++ src/hooks/useChangeValue.ts
@@ -0,0 +1,74 @@
+import { ChangeEvent } from 'react';
+
+import { useContext } from '@wordpress/element';
+
+import { useSetApi } from 'src/hooks/useSetApi';
+import { apiContext } from 'src/index';
+
+import { optionNameType } from 'src/types/ComponentsType';
+import { apiType, ChatToolsBaseType, ChatToolsItemKeyType, itemKeyType } from 'src/types/apiType';
+
+export const useChangeValue = ( itemKey: itemKeyType, optionName?: optionNameType ) => {
+	const { apiData, setApiData } = useContext( apiContext );
+
+	const changeValue = ( value: string | boolean | ChangeEvent< HTMLInputElement > ) => {
+		const newItem: apiType = JSON.parse( JSON.stringify( { ...apiData } ) );
+
+		const chatTools = ( toolName: ChatToolsItemKeyType ) => {
+			const isString = ( option: unknown ): option is string => {
+				return typeof option === 'string' ? true : false;
+			};
+			const isBaseOption = (
+				option: optionNameType
+			): option is keyof Omit< ChatToolsBaseType, 'log' > => {
+				return [ 'use', 'send_author', 'send_update' ].includes( option );
+			};
+
+			const isWebhook = ( option: optionNameType ): option is 'webhook_url' => {
+				return option === 'webhook_url';
+			};
+
+			if ( optionName && isBaseOption( optionName ) && typeof value === 'boolean' ) {
+				newItem[ toolName ][ optionName ] = value;
+			}
+
+			if ( optionName && isWebhook( optionName ) && isString( value ) ) {
+				newItem[ toolName as 'slack' | 'discord' ][ optionName ] = value;
+			}
+
+			if ( toolName === 'chatwork' && isString( value ) ) {
+				if ( optionName === 'api_token' ) {
+					newItem.chatwork.api_token = value;
+				}
+				if ( optionName === 'room_id' ) {
+					newItem.chatwork.room_id = value;
+				}
+			}
+		};
+
+		switch ( itemKey ) {
+			case 'slack':
+				chatTools( 'slack' );
+				break;
+			case 'discord':
+				chatTools( 'discord' );
+				break;
+			case 'chatwork':
+				chatTools( 'chatwork' );
+				break;
+			case 'cron_time':
+				if ( typeof value === 'object' ) {
+					newItem.cron_time = value.target.value;
+				}
+				break;
+			default:
+				break;
+		}
+
+		setApiData( newItem );
+	};
+
+	useSetApi( itemKey, apiData );
+
+	return { apiData, changeValue };
+};
