267d00fa2337f5cef08ea4ef0919434fc72db91b
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2022-04-29 16:11:52 +0900

    refs #280 fix translation en -> ja

--- src/components/atoms/ApiError.tsx
+++ src/components/atoms/ApiError.tsx
@@ -3,13 +3,23 @@ import { __ } from '@wordpress/i18n';
 
 export const ApiError = () => {
 	const messageList = [
-		__( 'Send Chat Toolsは、設定読み込みにWordPress REST APIを使用しています。', 'send-chat-tools' ),
-		__( 'もしこのエラーが続けて表示される場合は、WordPress REST APIが無効化されている可能性があります。', 'send-chat-tools' ),
-		__( 'WordPress REST APIはプラグインやテーマのfunctions.phpファイルで設定することができます。', 'send-chat-tools' ),
-		__( 'WordPress REST APIを有効化して再度アクセスしてください。', 'send-chat-tools' ),
+		__( 'Send Chat Tools uses the WordPress REST API for setting loading.', 'send-chat-tools' ),
+		__(
+			'If you continue to see this error, the WordPress REST API may be disabled.',
+			'send-chat-tools'
+		),
+		__(
+			// eslint-disable-next-line
+			"The WordPress REST API can be set up in a plugin or in your theme's functions.php file.",
+			'send-chat-tools'
+		),
+		__(
+			'Please activate the WordPress REST API and try accessing it again.',
+			'send-chat-tools'
+		),
 	];
 	return (
-		<Panel header={ __( 'API接続に失敗しました。', 'send-chat-tools' ) } >
+		<Panel header={ __( 'API connection failed.', 'send-chat-tools' ) } >
 			<PanelBody>
 				{ messageList.map( ( message, index ) => (
 					<PanelRow key={ index }>{ message }</PanelRow>
