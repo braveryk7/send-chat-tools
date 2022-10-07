369ba45f4246c073715b138fe1999e40ea0c54e6
braveryk7 <kenta-ousaka@ciexion.com>
2022-10-07 23:39:47 +0900

    refs #301 fix refactoring Sct_Base::deactivate args

--- send-chat-tools.php
+++ send-chat-tools.php
@@ -32,7 +32,11 @@ if ( ! isset( $get_php_version_bool ) ) {
 }
 
 if ( ! $get_php_version_bool->judgment( Sct_Base::get_required_php_version() ) ) {
-	$get_php_version_bool->deactivate( __FILE__, Sct_Base::get_plugin_name(), Sct_Base::get_required_php_version() );
+	$get_php_version_bool->deactivate(
+		__FILE__,
+		Sct_Base::get_plugin_name(),
+		Sct_Base::get_required_php_version()
+	);
 } else {
 	require_once dirname( __FILE__ ) . '/class/class-sct-encryption.php';
 	require_once dirname( __FILE__ ) . '/class/class-sct-admin-page.php';
