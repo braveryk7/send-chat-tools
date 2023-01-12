74a56adce6eef72c627955c9d5ef4a05ca948fde
braveryk7 <kenta-ousaka@ciexion.com>
2023-01-12 14:04:32 +0900

    refs #317 add instance properties

--- tests/SctPhpverJudge.php
+++ tests/SctPhpverJudge.php
@@ -5,5 +5,10 @@ declare( strict_types = 1 );
  * Test: Sct_Phpver_Judge
  */
 class SctPhpverJudgeTest extends PHPUnit\Framework\TestCase {
-
+	/**
+	 * This test class instance.
+	 *
+	 * @var object $instance instance.
+	 */
+	private $instance;
 }
