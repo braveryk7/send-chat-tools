0d752408ed3c2d9c7c385984850034a726c2b0e4
braveryk7 <kenta-ousaka@ciexion.com>
2023-02-26 16:21:53 +0900

    refs #327 fix Sct_Error_Mail use send_mail method

--- class/class-sct-base.php
+++ class/class-sct-base.php
@@ -320,7 +320,7 @@ class Sct_Base {
 				$send_mail->update_contents( $options );
 			} else {
 				$send_mail = new Sct_Error_Mail( $status_code, $id );
-				$send_mail->make_contents();
+				$send_mail->send_mail( ...$send_mail->make_contents() );
 			}
 		}
 
