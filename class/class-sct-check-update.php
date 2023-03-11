f770b46f625984e5b3e85b4f446a1d33d50b0d99
braveryk7 <kenta-ousaka@ciexion.com>
2023-03-11 13:36:18 +0900

    refs #409 refs #409 fix unused $key

--- class/class-sct-check-update.php
+++ class/class-sct-check-update.php
@@ -132,7 +132,7 @@ class Sct_Check_Update extends Sct_Base {
 
 		require_once ABSPATH . 'wp-admin/includes/plugin.php';
 		$current_plugins = get_plugins();
-		foreach ( $current_plugins as $key => $value ) {
+		foreach ( $current_plugins as $value ) {
 			if ( array_key_exists( $value['Name'], self::PLUGIN_OPTION_NAME ) ) {
 				$get_update = get_option( self::PLUGIN_OPTION_NAME[ $value['Name'] ] );
 				if ( ! empty( $get_update ) && version_compare( $value['Version'], $get_update->update->version, '<' ) ) {
