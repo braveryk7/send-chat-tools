e8df840f0234c1dd25e7edcf1a01efe6275d0f15
braveryk7 <kenta-ousaka@ciexion.com>
2023-03-24 00:00:12 +0900

    refs #311 fix variable name

--- class/class-sct-base.php
+++ class/class-sct-base.php
@@ -299,15 +299,15 @@ class Sct_Base {
 		if ( 200 !== $status_code && 204 !== $status_code ) {
 			require_once dirname( __FILE__ ) . '/class-sct-error-mail.php';
 			if ( 'update' === $id ) {
-				$send_mail = new Sct_Error_Mail( $status_code, $id, $tools );
+				$send_mail = new Sct_Error_Mail( $status_code, $id, $tool );
 				$send_mail->send_mail( ...$send_mail->update_contents( $options['plain_data'] ) );
 			} else {
-				$send_mail = new Sct_Error_Mail( $status_code, $id, $tools );
+				$send_mail = new Sct_Error_Mail( $status_code, $id, $tool );
 				$send_mail->send_mail( ...$send_mail->generate_contents() );
 			}
 		}
 
-		return $this->logger( $status_code, $tools, $id );
+		return $this->logger( $status_code, $tool, $id );
 	}
 
 	/**
