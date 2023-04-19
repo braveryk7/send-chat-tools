5e8717df613ab744b29ad020a4244037ca46fb56
braveryk7 <kenta-ousaka@ciexion.com>
2023-04-20 01:57:22 +0900

    refs #448 add get_send_status method

--- class/class-sct-check-comment.php
+++ class/class-sct-check-comment.php
@@ -29,4 +29,42 @@ class Sct_Check_Comment extends Sct_Base {
 		$tools       = [ 'slack', 'discord', 'chatwork' ];
 		$comment     = get_comment( $comment_id );
 	}
+
+	/**
+	 * Check send status.
+	 *
+	 * @param string $tool_name       Tool name.
+	 * @param array  $tools           Tool options -> sct_options[TOOLNAME].
+	 * @param string $comment_user_id Comment user id.
+	 */
+	private function get_send_status( string $tool_name, array $tools, string $comment_user_id ): bool {
+		$status = [
+			'use'    => false,
+			'api'    => false,
+			'author' => false,
+		];
+
+		$status['use'] = $tools['use'] ? true : false;
+
+		$status['author'] = ! $tools['send_author'] || $tools['send_author'] && '0' === $comment_user_id ? true : false;
+
+		switch ( $tool_name ) {
+			case 'slack':
+			case 'discord':
+				$api           = $tools['webhook_url'];
+				$status['api'] = ! empty( $api ) ? true : false;
+				break;
+			case 'chatwork':
+				$api           = [
+					'api_token' => $tools['api_token'],
+					'room_id'   => $tools['room_id'],
+				];
+				$status['api'] = ! empty( $api['api_token'] ) && ! empty( $api['room_id'] ) ? true : false;
+				break;
+			default:
+				$status['api'] = false;
+		}
+
+		return in_array( false, $status, true ) ? false : true;
+	}
 }
