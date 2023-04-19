4138d394a891846b2e070f2731c077c8cbf12268
braveryk7 <kenta-ousaka@ciexion.com>
2023-04-20 01:55:47 +0900

    refs #448 add require vars

--- class/class-sct-check-comment.php
+++ class/class-sct-check-comment.php
@@ -18,4 +18,15 @@ if ( ! defined( 'ABSPATH' ) ) {
  * If comment data exists, a class that calls the class that performs various processes.
  */
 class Sct_Check_Comment extends Sct_Base {
+
+	/**
+	 * Controller called by add_action.
+	 *
+	 * @param int $comment_id     Comment ID.
+	 */
+	public function controller( int $comment_id ) {
+		$sct_options = $this->get_sct_options();
+		$tools       = [ 'slack', 'discord', 'chatwork' ];
+		$comment     = get_comment( $comment_id );
+	}
 }
