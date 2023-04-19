752fb3ec423113842c21c7edd5aae6e39ea96f1d
braveryk7 <kenta-ousaka@ciexion.com>
2023-04-20 01:50:38 +0900

    refs #448 initial commit

---
+++ class/class-sct-check-comment.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * If comment data exists, a class that calls the class that performs various processes.
+ *
+ * @author Ken-chan
+ * @package WordPress
+ * @subpackage Send Chat Tools
+ * @since 1.5.0
+ */
+
+declare( strict_types = 1 );
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit( 'You do not have access rights.' );
+}
