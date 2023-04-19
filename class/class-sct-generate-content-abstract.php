b1c246759ab62b572d649d33067407da03523738
braveryk7 <kenta-ousaka@ciexion.com>
2023-04-20 01:45:45 +0900

    refs #448 initial commit

---
+++ class/class-sct-generate-content-abstract.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Abstract class for generating content data and processing it in each chat tool
+ *
+ * @author Ken-chan
+ * @package WordPress
+ * @subpackage Send Chat Tools
+ * @since 1.5.0
+ */
+
+declare( strict_types = 1 );
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit( 'You do not have access rights.' );
+}
