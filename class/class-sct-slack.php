8e29d030753f8b1aa00b404d5313310b0a2dc23b
braveryk7 <kenta-ousaka@ciexion.com>
2023-04-20 14:26:15 +0900

    refs #448 initial commit

---
+++ class/class-sct-slack.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Class for Slack processing.
+ *
+ * @author Ken-chan
+ * @package WordPress
+ * @subpackage Send Chat Tools
+ * @since 1.5.0
+ */
+
+declare( strict_types = 1 );
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit( 'You do not have access rights.' );
+}
