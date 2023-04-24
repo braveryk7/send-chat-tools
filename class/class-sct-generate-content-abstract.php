ec57ccfe15873e8eb55c5886441fa7771b63e886
braveryk7 <kenta-ousaka@ciexion.com>
2023-04-25 00:20:27 +0900

    refs #471 add login notify message

--- class/class-sct-generate-content-abstract.php
+++ class/class-sct-generate-content-abstract.php
@@ -165,7 +165,7 @@ abstract class Sct_Generate_Content_Abstract extends Sct_Base {
 	 */
 	protected function get_send_text( string $type, string $param ): string {
 		$message = [
-			'comment'    => [
+			'comment'      => [
 				'title'      => esc_html__( 'new comment has been posted.', 'send-chat-tools' ),
 				'article'    => esc_html__( 'Commented article:', 'send-chat-tools' ),
 				'author'     => esc_html__( 'Author:', 'send-chat-tools' ),
@@ -178,12 +178,12 @@ abstract class Sct_Generate_Content_Abstract extends Sct_Base {
 				'click'      => esc_html__( 'Click here to approve', 'send-chat-tools' ),
 				'spam'       => esc_html__( 'Spam', 'send-chat-tools' ),
 			],
-			'update'     => [
+			'update'       => [
 				'title'  => esc_html__( 'Notification of new updates.', 'send-chat-tools' ),
 				'update' => esc_html__( 'Please login to the admin panel to update.', 'send-chat-tools' ),
 				'page'   => esc_html__( 'Update Page:', 'send-chat-tools' ),
 			],
-			'dev_notify' => [
+			'dev_notify'   => [
 				/* translators: 1: Theme or Plugin name */
 				'title'   => esc_html__( 'Update notifications from %s', 'send-chat-tools' ),
 				'website' => esc_html__( 'Official Web Site', 'send-chat-tools' ),
@@ -192,6 +192,15 @@ abstract class Sct_Generate_Content_Abstract extends Sct_Base {
 					'If this message is sent by a malicious developer, it can be rejected with the following key',
 					'send-chat-tools',
 				),
+			],
+			'login_notify' => [
+				'title'              => __( 'Login Notification', 'send-chat-tools' ),
+				'user_name'          => __( 'User name:', 'send-chat-tools' ),
+				'date'               => __( 'Date and time:', 'send-chat-tools' ),
+				'login_env'          => __( 'Login environment:', 'send-chat-tools' ),
+				'ip_address'         => __( 'IP Address:', 'send-chat-tools' ),
+				'unauthorized_login' => __( 'If you do not recognize this message, you may have an unauthorized login.' ),
+				'disconnect'         => __( 'Disconnect all location sessions and change passwords.' ),
 			],
 		];
 
