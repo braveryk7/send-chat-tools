4d451221f617449cfd4aa949b846e27f37a162eb
braveryk7 <kenta-ousaka@ciexion.com>
2023-04-24 15:40:11 +0900

    refs #471 initial commit

---
+++ class/class-sct-check-login.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * A class that detects user login and calls a class for sending when the condition is met.
+ *
+ * @author Ken-chan
+ * @package WordPress
+ * @subpackage Send Chat Tools
+ * @since 1.5.0
+ */
+
+declare( strict_types = 1 );
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit( 'You do not have access rights.' );
+}
+
+/**
+ * A class that detects user login and calls a class for sending when the condition is met.
+ */
+class Sct_Check_Login {
+
+}
