e6e2b2115270b68bfccc244c3c5a1829b5c8d1f8
braveryk7 <kenta-ousaka@ciexion.com>
2023-04-25 18:25:04 +0900

    refs #471 add controller method

--- class/class-sct-check-login.php
+++ class/class-sct-check-login.php
@@ -24,4 +24,32 @@ class Sct_Check_Login extends Sct_Base {
 	public function __construct() {
 		add_action( 'wp_login', [ $this, 'controller' ], 10, 2 );
 	}
+
+	/**
+	 * Method called when a user logs in.
+	 *
+	 * @param string $user_login User name.
+	 * @param object $user       User object.
+	 */
+	public function controller( $user_login, $user ) {
+		$sct_options = $this->get_sct_options();
+		$tools       = [ 'slack', 'discord', 'chatwork' ];
+
+		foreach ( $tools as $tool ) {
+			$api_column = 'chatwork' === $tool ? 'api_token' : 'webhook_url';
+
+			if ( $sct_options[ $tool ]['use'] && $sct_options[ $tool ]['login_notify'] ) {
+				$instance = match ( $tool ) {
+					'slack'    => Sct_Slack::get_instance(),
+					'discord'  => Sct_Discord::get_instance(),
+					'chatwork' => Sct_Chatwork::get_instance(),
+				};
+				$instance?->generate_login_message( $user )?->generate_header()?->send_tools( 'login_notify', $tool );
+			} elseif ( $sct_options[ $tool ]['use'] && empty( $sct_options[ $tool ][ $api_column ] ) ) {
+				$this->logger( 1001, $tool, '1' );
+			} elseif ( 'chatwork' === $tools && ( $sct_options[ $tool ]['use'] && empty( $sct_options[ $tool ]['room_id'] ) ) ) {
+				$this->logger( 1002, 'chatwork', '1' );
+			};
+		}
+	}
 }
