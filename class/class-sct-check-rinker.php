1b484baea3c0888055abb10a7b2425d38ab6a087
braveryk7 <kenta-ousaka@ciexion.com>
2023-04-27 00:38:50 +0900

    refs #32 initial commit

---
+++ class/class-sct-check-rinker.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * A class that checks Rinker's out-of-stock information and calls the class for transmission when the condition is met.
+ *
+ * @author Ken-chan
+ * @package WordPress
+ * @subpackage Send Chat Tools
+ * @since 1.5.0
+ */
+
+declare( strict_types = 1 );
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit( 'You do not have access rights.' );
+}
+
+/**
+ * A class that checks Rinker's out-of-stock information and calls the class for transmission when the condition is met.
+ */
+class Sct_Check_Rinker extends Sct_Base {
+}
