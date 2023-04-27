f60841783fb7ead8e182bd409181baeaa51604d9
braveryk7 <kenta-ousaka@ciexion.com>
2023-04-27 15:35:34 +0900

    refs #32 add check_rinker_exists_items_cron

--- src/hooks/useChangeValue.ts
+++ src/hooks/useChangeValue.ts
@@ -55,9 +55,13 @@ export const useChangeValue = ( itemKey: itemKeyType, optionName?: optionNameTyp
 			chatTools( 'discord' );
 		} else if ( itemKey === 'chatwork' ) {
 			chatTools( 'chatwork' );
-		} else if ( itemKey === 'cron_time' ) {
+		} else if ( itemKey === 'cron_time' || 'check_rinker_exists_items_cron' ) {
 			if ( typeof value === 'object' && 'target' in value ) {
-				newItem.cron_time = value.target.value;
+				if ( value.target.id === 'cron_time' ) {
+					newItem.cron_time = value.target.value;
+				} else if ( value.target.id === 'check_rinker_exists_items_cron' ) {
+					newItem.check_rinker_exists_items_cron = value.target.value;
+				}
 			}
 		} else if ( itemKey === 'ignore_key' ) {
 			if ( Array.isArray( value ) ) {
