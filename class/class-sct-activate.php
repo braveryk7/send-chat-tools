123c927d9067ba8c8b91fe63d399f5552fe8bfe2
braveryk7 <kenta-ousaka@ciexion.com>
2023-04-27 14:06:20 +0900

    refs #32 add rinker_notify

--- class/class-sct-activate.php
+++ class/class-sct-activate.php
@@ -33,21 +33,23 @@ class Sct_Activate extends Sct_Base {
 	 */
 	public function register_options(): void {
 		$chat_tools_value = [
-			'use'          => false,
-			'webhook_url'  => '',
-			'send_author'  => false,
-			'send_update'  => false,
-			'login_notify' => false,
-			'log'          => [],
+			'use'           => false,
+			'webhook_url'   => '',
+			'send_author'   => false,
+			'send_update'   => false,
+			'login_notify'  => false,
+			'rinker_notify' => false,
+			'log'           => [],
 		];
 		$chatwork_value   = [
-			'use'          => false,
-			'api_token'    => '',
-			'room_id'      => '',
-			'send_author'  => false,
-			'send_update'  => false,
-			'login_notify' => false,
-			'log'          => [],
+			'use'           => false,
+			'api_token'     => '',
+			'room_id'       => '',
+			'send_author'   => false,
+			'send_update'   => false,
+			'login_notify'  => false,
+			'rinker_notify' => false,
+			'log'           => [],
 		];
 
 		$options = [
