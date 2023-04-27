0bb063e229298dc5755b4ffd52edbb09ff5b4260
braveryk7 <kenta-ousaka@ciexion.com>
2023-04-27 13:13:30 +0900

    refs #32 add check_rinker_exists_items method

--- class/class-sct-check-rinker.php
+++ class/class-sct-check-rinker.php
@@ -18,4 +18,32 @@ if ( ! defined( 'ABSPATH' ) ) {
  * A class that checks Rinker's out-of-stock information and calls the class for transmission when the condition is met.
  */
 class Sct_Check_Rinker extends Sct_Base {
+	/**
+	 * Check Amazon and Rakuten for discontinued products for Rinker.
+	 */
+	public function check_rinker_exists_items(): array {
+		$exists_items = [
+			'amazon'  => [],
+			'rakuten' => [],
+		];
+
+		foreach ( [ 'amazon', 'rakuten' ] as $shop ) {
+			$query = new WP_Query(
+				[
+					'post_type'      => 'yyi_rinker',
+					'meta_key'       => 'yyi_rinker_is_' . $shop . '_no_exist', //phpcs:ignore
+					'meta_value_num' => 1,
+					'posts_per_page' => -1,
+				],
+			);
+
+			while ( $query->have_posts() ) {
+				$query->the_post();
+				$exists_items[ $shop ][ get_the_ID() ] = get_post();
+			}
+			wp_reset_postdata();
+		}
+
+		return $exists_items;
+	}
 }
