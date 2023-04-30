1f613730736e66af2a1cb2417a4a96006b733f13
braveryk7 <kenta-ousaka@ciexion.com>
2023-04-30 17:47:17 +0900

    refs #530 fix require Sct_Generate_Content_Abstract, remove instance

--- tests/SctErrorMailTest.php
+++ tests/SctErrorMailTest.php
@@ -20,6 +20,7 @@ class SctErrorMailTest extends PHPUnit\Framework\TestCase {
 			define( 'ABSPATH', '' );
 		}
 
+		require_once './class/class-sct-generate-content-abstract.php';
 		require_once './class/class-sct-error-mail.php';
 		require_once './tests/lib/wordpress-functions.php';
 	}
@@ -29,10 +30,6 @@ class SctErrorMailTest extends PHPUnit\Framework\TestCase {
 	 * Create instance.
 	 */
 	protected function setUp() :void {
-		$error_code     = 1;
-		$comment_id     = '1';
-		$tool_name      = 'slack';
-		$this->instance = new Sct_Error_Mail( $error_code, $comment_id, $tool_name );
 	}
 
 	/**
