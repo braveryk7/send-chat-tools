657b6269bac278c7c032a1bc28c69ba0b5b4b285
braveryk7 <kenta-ousaka@ciexion.com>
2023-04-30 17:27:13 +0900

    refs #530 fix implementation generate_login_message

--- class/class-sct-error-mail.php
+++ class/class-sct-error-mail.php
@@ -164,6 +164,31 @@ class Sct_Error_Mail extends Sct_Generate_Content_Abstract {
 	 * Generate login notify for Error Mail.
 	 */
 	public function generate_login_message(): Sct_Error_Mail {
+		$this->mail_title = $this->get_send_text( 'login_notify', 'title' );
+
+		$user_name       = $this->original_data->data->user_login;
+		$user_email      = $this->original_data->data->user_email;
+		$login_user_name = $this->get_send_text( 'login_notify', 'user_name' ) . ": {$user_name}<$user_email>";
+
+		$now_date   = gmdate( 'Y-m-d H:i:s', strtotime( current_datetime()->format( 'Y-m-d H:i:s' ) ) );
+		$login_date = $this->get_send_text( 'constant', 'date' ) . ": {$now_date}";
+
+		$os_browser = getenv( 'HTTP_USER_AGENT' );
+		$login_env  = $this->get_send_text( 'login_notify', 'login_env' ) . ": {$os_browser}";
+
+		$ip_address       = getenv( 'REMOTE_ADDR' );
+		$login_ip_address = $this->get_send_text( 'login_notify', 'ip_address' ) . ": {$ip_address}";
+
+		$this->content =
+			$this->site_name . '(' . $this->site_url . ') ' . $this->mail_title . "\n\n" .
+			$login_user_name . "\n" . $login_date . "\n" . $login_env . "\n" . $login_ip_address . "\n\n" .
+			$this->get_send_text( 'login_notify', 'unauthorized_login' ) . "\n" .
+			$this->get_send_text( 'login_notify', 'disconnect' ) . "\n" .
+			$this->site_url . '/wp-admin/profile.php' . "\n\n" .
+			$this->generate_context( 'error_mail' ) . "\n" .
+			esc_html__( 'Tool name:', 'send-chat-tools' ) . ucfirst( $this->tool_name ) . "\n" .
+			esc_html__( 'Error code:', 'send-chat-tools' ) . $this->error_code;
+
 		return $this;
 	}
 
