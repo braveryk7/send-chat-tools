0a06228e2a25f1405dc565d757c0e16671c3fd8f
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2023-05-04 01:50:58 +0900

    refs #549 rm isOptionNameRinker function

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -1,11 +1,5 @@
-import { ChatToolsBaseType } from 'src/types/apiType';
-
 export const PREFIX = 'sct';
 
 export const addPrefix = ( value: string ): string => {
 	return PREFIX + '_' + value;
-};
-
-export const isOptionNameRinker = ( optionName: Exclude<keyof ChatToolsBaseType, 'log'> ) => {
-	return 'rinker_notify' === optionName ? true : false;
 };
