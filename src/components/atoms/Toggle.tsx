54a43f7cc33bf196a3245bebde196430426034a3
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2023-05-04 01:29:17 +0900

    refs #549 fix use rinkerUtils

--- src/components/atoms/Toggle.tsx
+++ src/components/atoms/Toggle.tsx
@@ -5,6 +5,7 @@ import { __ } from '@wordpress/i18n';
 
 import { useChangeValue } from 'src/hooks/useChangeValue';
 import { apiContext } from 'src/index';
+import { isOptionNameRinker, rinkerMessages } from 'src/utils/rinkerUtils';
 
 import { TogglePropsType } from 'src/types/ComponentsType';
 
@@ -12,18 +13,12 @@ export const Toggle = ( props: TogglePropsType ) => {
 	const { itemKey, optionName, label } = props;
 	const { apiData, changeValue } = useChangeValue( itemKey, optionName );
 	const { isRinkerActivated } = useContext( apiContext );
-
-	const isOptionNameRinker = () => {
-		return 'rinker_notify' === optionName ? true : false;
-	};
-
-	const rinkerExistMessage = __( 'Rinker is not activated.', 'send-chat-tools' );
-	const rinkerRecommendMessage = __(
-		'Rinker is a very popular product management plug-in for Amazon and Rakuten.',
-		'send-chat-tools'
-	);
-	const rinkerUrl = 'https://oyakosodate.com/rinker/';
-	const rinkerFanboxUrl = 'https://oyayoi.fanbox.cc/';
+	const [
+		rinkerExistMessage,
+		rinkerRecommendMessage,
+		rinkerUrl,
+		rinkerFanboxUrl,
+	] = rinkerMessages();
 
 	return (
 		<>
@@ -31,29 +26,33 @@ export const Toggle = ( props: TogglePropsType ) => {
 				<ToggleControl
 					label={ label }
 					checked={ apiData[ itemKey ][ optionName ] }
-					disabled={ isOptionNameRinker() && ! isRinkerActivated }
+					disabled={ isOptionNameRinker( optionName ) && ! isRinkerActivated }
 					onChange={ ( value ) => {
 						changeValue( value );
 					} }
 				/>
 			}
 			{
-				isOptionNameRinker() && ! isRinkerActivated &&
+				isOptionNameRinker( optionName ) && ! isRinkerActivated &&
 					<div className="sct-rinker-notice">
 						<p>
 							{ rinkerExistMessage + ' ' + rinkerRecommendMessage } <br />
 							{ __( 'Download', 'send-chat-tools' ) + ' >>> ' }
-							<ExternalLink
-								href={ rinkerUrl }
-							>
-								{ __( 'Rinker Official Web Site', 'send-chat-tools' ) }
-							</ExternalLink>
+							{ 'string' === typeof rinkerUrl &&
+								<ExternalLink
+									href={ rinkerUrl }
+								>
+									{ __( 'Rinker Official Web Site', 'send-chat-tools' ) }
+								</ExternalLink>
+							}
 							{ ' / ' }
-							<ExternalLink
-								href={ rinkerFanboxUrl }
-							>
-								{ __( 'Rinker Official FANBOX', 'send-chat-tools' ) }
-							</ExternalLink>
+							{ 'string' === typeof rinkerFanboxUrl &&
+								<ExternalLink
+									href={ rinkerFanboxUrl }
+								>
+									{ __( 'Rinker Official FANBOX', 'send-chat-tools' ) }
+								</ExternalLink>
+							}
 						</p>
 					</div>
 			}
