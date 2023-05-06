545874da7c3d1053cd7d41e2765c49e23dac6166
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2023-05-06 14:01:15 +0900

    refs #557 fix variable name

--- class/class-sct-check-rinker.php
+++ class/class-sct-check-rinker.php
@@ -95,18 +95,18 @@ class Sct_Check_Rinker extends Sct_Base {
 	 */
 	public function check_cron_time(): void {
 		if ( $this->is_rinker_activated() ) {
-			$get_next_schedule     = wp_get_scheduled_event( $this->cron_event_name );
-			$sct_options           = $this->get_sct_options();
-			$datetime_string       = gmdate( 'Y-m-d ' . $sct_options['rinker_cron_time'], strtotime( current_datetime()->format( 'Y-m-d H:i:s' ) ) );
-			$sct_options_timestamp = strtotime( -1 * (int) current_datetime()->format( 'O' ) / 100 . 'hour', strtotime( $datetime_string ) );
+			$get_next_schedule  = wp_get_scheduled_event( $this->cron_event_name );
+			$sct_options        = $this->get_sct_options();
+			$datetime_string    = gmdate( 'Y-m-d ' . $sct_options['rinker_cron_time'], strtotime( current_datetime()->format( 'Y-m-d H:i:s' ) ) );
+			$datetime_timestamp = strtotime( -1 * (int) current_datetime()->format( 'O' ) / 100 . 'hour', strtotime( $datetime_string ) );
 			update_option( 'sct_cron', gettype( $datetime_string ) );
 
 			if ( ! $get_next_schedule ) {
-				wp_schedule_event( $sct_options_timestamp, 'daily', $this->cron_event_name );
+				wp_schedule_event( $datetime_timestamp, 'daily', $this->cron_event_name );
 			} else {
 				if ( isset( $sct_options['cron_time'] ) ) {
-					if ( $get_next_schedule->timestamp !== $sct_options_timestamp ) {
-						$sct_options_timestamp <= time() ? $sct_options_timestamp = strtotime( '+1 day', $sct_options_timestamp ) : $sct_options_timestamp;
+					if ( $get_next_schedule->timestamp !== $datetime_timestamp ) {
+						$datetime_timestamp <= time() ? $sct_options_timestamp = strtotime( '+1 day', $datetime_timestamp ) : $datetime_timestamp;
 						wp_clear_scheduled_hook( $this->cron_event_name );
 						wp_schedule_event( $sct_options_timestamp, 'daily', $this->cron_event_name );
 					}
